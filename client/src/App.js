4ea24476c26a851d8aedbbbedf8fbe9aea84e64c
Thomas Dawson <dawsontc003@gmail.com>
2021-03-03 16:40:51 -0700

    rendering home page with login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import NewRecipe from "./pages/NewRecipe";
+import NewRecipe from "./pages/Home";
 import "tailwindcss/tailwind.css";
 
 function App() {
---
+++ client/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <label>Email:</label>
+        <input></input>
+        <label>Password:</label>
+        <input></input>
+        <button>Submit</button>
+      </form>
+
+      <button>Signup</button>
+    </div>
+  );
+}
+
+export default Home;
