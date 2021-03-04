888c065173b726c3d999182c0c46c8ed05b78ee9
Shane McCarthy <shane.mccarthy@1888investments.com>
2021-03-04 10:23:12 -0700

    auth files

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+googleClientID:"659871250507-bokvnt4700ki1lu59kp9h50ccro3g721.apps.googleusercontent.com",
+googleClientSecret:"U_39wEznAZDNeAwLT4aeBRWf"
+
+}
--- server.js
+++ server.js
@@ -1,10 +1,38 @@
 const express = require("express");
+const passport = require("passport");
 const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile) => {
+      console.log(accessToken);
+      console.log(refreshToken);
+      console.log(profile);
+    }
+  )
+);
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
+app.listen(PORT);
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
