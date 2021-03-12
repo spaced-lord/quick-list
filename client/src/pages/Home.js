42f16a1a7c5cc7363062f0bb1f3a5b63e81d5100
Thomas Dawson <dawsontc003@gmail.com>
2021-03-11 20:52:36 -0700

    all page background

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -15,15 +15,13 @@ function Home(props) {
       <h2 className="aside">Have a Grocery List in just a few Clicks! </h2>
 
       <p>
-        It is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layout. The point of
-        using Lorem Ipsum is that it has a more-or-less normal distribution of
-        letters, as opposed to using 'Content here, content here', making it
-        look like readable English. Many desktop publishing packages and web
-        page editors now use Lorem Ipsum as their default model text, and a
-        search for 'lorem ipsum' will uncover many web sites still in their
-        infancy. Various versions have evolved over the years, sometimes by
-        accident, sometimes on purpose (injected humour and the like).
+        Quick-list wants to make your life super easy and as stress free as
+        possible during these times of COVID! This application will help you to
+        sync up your favorite meals, the ingredients you need make them, to
+        create a grocery list that allows you to get in and out of the grocery
+        store quickly. Find that grocery item, delete it as you cruise the store
+        aisles. Store that new favorite recipe for use in the future. This
+        application is your friend in so many ways!
       </p>
     </div>
   );
--- client/src/pages/NewRecipe.js
+++ client/src/pages/NewRecipe.js
@@ -200,7 +200,7 @@ const NewRecipe = () => {
 
   //Return for the page
   return (
-    <div className="py-8 px-8 m-40 bg-green-300 text-white font-bold text-center">
+    <div className="py-8 px-8 m-40 bg-green-300 font-bold text-center ">
       {id ? (
         <div className="recipeform">
           <form onSubmit={submitFunction}>
